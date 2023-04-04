9880e54b6fdadfbaa83d5972f30a05352f163b58
IsabelleCruz333 <isabellecruz333@gmail.com>
2023-04-04 11:28:26 -0300

    projeto-javascript-3

---
+++ script.js
@@ -0,0 +1,49 @@
+//let opcao=2
+/*let opcao =parseInt(prompt("digite uma opção"))
+
+switch(opcao){
+    case 1:
+        document.write("primeira opção")
+        break;
+    case 2:
+        document.write("segunda opção")
+        break;
+    case 3:
+        document.write("terceira opção")
+        break;
+    default:
+        document.write("nenhuma das opções válidas")
+        break;
+}*/
+
+//incremento e decremento
+
+/*let valor= 7
+
+document.write(valor)
+document.write("<br><br>")
+valor = valor +=4;
+document.write(valor)
+
+let valor2= 10
+document.write(valor2)
+document.write("<br><br>")
+valor2 = valor2 -=1
+document.write(valor2)*/
+
+//usando o for
+
+for(let i=0; i<10; i++){
+    document.write("o valor é " ,i)
+    document.write("<br><br>")
+}
+
+//usando for com array
+
+let carros=["gol","fusca","audi","corsa"]
+for(let i=0; i < carros.length; i++){
+    document.write(carros[i])
+    document.write("<br><br>")
+}
+
+
